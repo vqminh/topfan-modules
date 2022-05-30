530250c9b4913ff0e64704ade502fb30dee9a8e6
Minh Vu <mivu@linkedin.com>
2022-05-29 23:14:55 -0700

    m

---
+++ utils/logging.ts
@@ -0,0 +1,40 @@
+import { API_URL, DEV, IS_SERVER } from "@settings/site-settings";
+import { auth } from "./firebase";
+import axios from "axios";
+
+export function LOGGER(...args: any[]) {
+  if (DEV) {
+    args?.forEach(console.log);
+  }
+}
+
+export function reportError(...data: any[]) {
+  if (!DEV && !IS_SERVER) {
+    axios
+      .create({
+        baseURL: API_URL,
+        timeout: 1000,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+      .post("/error", {
+        data,
+        location: window.location.href,
+        uid: auth.currentUser?.uid,
+        displayName: auth.currentUser?.displayName,
+        email: auth.currentUser?.email,
+      })
+      .catch(console.error);
+  } else {
+    data?.forEach(console.error);
+  }
+}
+
+export class ValidationError extends Error {
+  constructor(name: string, message: string) {
+    super(message);
+    this.name = name;
+  }
+}
